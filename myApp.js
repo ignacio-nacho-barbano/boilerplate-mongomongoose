348d5ca39e455fa1f7182e9107a32adaecc2f75a
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2024-01-25 19:14:53 -0300

    try removing manual connect

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 const personSchema = new mongoose.Schema({
   name: {
