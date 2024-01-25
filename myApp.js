d7e93c5893d098b7510825007d902f9e46e21a23
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2024-01-25 20:09:38 -0300

    restore db connection config

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
